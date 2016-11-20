718a7e8e9e1912d91d281038fb7c033221a1fc0c
Darwin Morales <dmorales@infomedia.com.au>
2016-11-20 18:13:28 +1100

    test

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var MyButtonController = require('./components/MyButtonController');
-
+// test
 ReactDOM.render(
   <MyButtonController/>,
   document.querySelector('#example')
